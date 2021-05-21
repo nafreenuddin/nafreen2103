64acf4e9b556a315faddf7b907e2d3488d679ec9
Shaik Nafreenuddin <Shaik Nafreenuddin@LAPTOP-31NQL4HF>
2021-05-21 15:10:22 +0530

    my first program

---
+++ core_java/src/main/java/com/swithprogram.java
@@ -0,0 +1,16 @@
+package com;
+
+public class swithprogram {
+	public static void main(String[] args) {
+		char color = 'g';
+		switch(color) {
+		case 'r':System.out.println("Red");
+		break;
+		case 'g': System.out.println("Green");
+		break;
+		case 'w' : System.out.println("yello");
+		break;
+		default : System.out.println("No color");
+		}
+	}
+}
